566208f31b7907d18d06d6567a575c4c956d628d
WilsonCamacho <wilsoncamacho60@hotmail.com>
2023-06-06 14:38:51 -0500

    Add front-end

---
+++ FrontEnd/src/app/models/users.ts
@@ -0,0 +1,8 @@
+export class Users {
+    idIdentifier: number | undefined;
+    name: string | undefined;
+    lastName: string | undefined;
+    email:string | undefined;
+    phoneNumber:number | undefined;
+    dateOfBirthday:any;
+}
